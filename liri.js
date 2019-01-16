24f1a67eed032f7d87a8c8e12e18378c07326bf1
Michael Fox <mikefox17@gmail.com>
2019-01-15 20:38:35 -0500

    added concert functions

--- liri.js
+++ liri.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
-
+//OH SO MANY VAR's
 var keys = require("./keys.js");
 var request = require("request");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
+var moment = require("moment");
+//VAR's for user action and inputs
 var input = process.argv;
 var action = input[2];
 var inputs = input[3];
-
+//Switches for user actions
 switch (action) {
   case "spotify-this-song":
     spotify(inputs);
@@ -25,26 +27,50 @@ switch (action) {
     doIt(inputs);
     break;
 }
-
+//OMDB API CALL
 function movie(inputs) {
   var queryUrl =
-    "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=40e9cece";
+    "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=trilogy";
 
   request(queryUrl, function(error, response, body) {
     if (!inputs) {
       inputs = "Mr Nobody";
     }
+    //Shorthand for console logging the results
+    var results = JSON.parse(body);
+    //If no error than results are displayed
     if (!error && response.statusCode === 200) {
-      console.log("Title: " + JSON.parse(body).Title);
-      console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Title: " + results.Title);
+      console.log("Release Year: " + results.Year);
+      console.log("IMDB Rating: " + results.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + results.Ratings[1].Value);
+      console.log("Country: " + results.Country);
+      console.log("Language: " + results.Language);
+      console.log("Plot: " + results.Plot);
+      console.log("Actors: " + results.Actors);
+    }
+  });
+}
+//Concert API call
+function concert(inputs) {
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    inputs +
+    "/events?app_id=codingbootcamp";
+
+  request(queryUrl, function(error, response, body) {
+    if (!inputs) {
+      inputs = "Not today, Jack";
+    }
+    //Shorthand for console logging results
+    var result = JSON.parse(body)[0];
+    if (!error && response.statusCode === 200) {
+      console.log("City: " + result.venue.city);
+      console.log("Venue Name: " + result.venue.name);
+      //moment.js for formatting the date
       console.log(
-        "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value
+        "Date of Event: " + moment(result.datetime).format("MM/DD/YYYY")
       );
-      console.log("Country: " + JSON.parse(body).Country);
-      console.log("Language: " + JSON.parse(body).Language);
-      console.log("Plot: " + JSON.parse(body).Plot);
-      console.log("Actors: " + JSON.parse(body).Actors);
     }
   });
 }
